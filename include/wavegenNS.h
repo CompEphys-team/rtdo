ffb1fe2012cb370bfc3b53b8c7eee58e87d72f02
Felix Kern <kernfel+github@gmail.com>
2016-03-02 13:52:53 +0000

    Added missing header

--- include/wavegenNS.h
+++ include/wavegenNS.h
@@ -1,3 +1,15 @@
+/*--------------------------------------------------------------------------
+Author: Felix Kern
+
+Institute: School of Life Sciences
+University of Sussex
+Falmer, Brighton BN1 9QG, UK
+
+email to:  fbk21@sussex.ac.uk
+
+initial version: 2016-02-16
+
+--------------------------------------------------------------------------*/
 #ifndef WAVEGENNS_H
 #define WAVEGENNS_H
 #include <iostream>
