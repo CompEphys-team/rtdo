3965bc6912756751d62dc50ddce241f9a5ed4ccd
Felix Kern <kernfel+github@gmail.com>
2017-03-21 14:21:02 +0000

    Added MAPElite QDataStream i/o functions

--- src/core/streamops.cpp
+++ src/core/streamops.cpp
@@ -13,3 +13,73 @@ std::ostream &operator<<(std::ostream &os, const QString &str)
     os << str.toStdString();
     return os;
 }
+
+QDataStream &operator<<(QDataStream &os, const MAPElite &e)
+{
+    os << e.wave << e.stats;
+    os << quint32(e.bin.size());
+    for ( const size_t &b : e.bin )
+        os << quint32(b);
+    return os;
+}
+
+QDataStream &operator>>(QDataStream &is, MAPElite &e)
+{
+    is >> e.wave >> e.stats;
+    quint32 bins, val;
+    is >> bins;
+    e.bin.resize(bins);
+    for ( size_t &b : e.bin ) {
+        is >> val;
+        b = size_t(val);
+    }
+    return is;
+}
+
+QDataStream &operator<<(QDataStream &os, const Stimulation &stim)
+{
+    os << double(stim.baseV) << double(stim.duration) << double(stim.tObsBegin) << double(stim.tObsEnd);
+    os << quint32(stim.size());
+    for ( const Stimulation::Step &s : stim )
+        os << s.ramp << double(s.t) << double(s.V);
+    return os;
+}
+
+QDataStream &operator>>(QDataStream &is, Stimulation &stim)
+{
+    double baseV, duration, tObsBegin, tObsEnd, t, V;
+    quint32 steps;
+    is >> baseV >> duration >> tObsBegin >> tObsEnd;
+    stim.baseV = baseV;
+    stim.duration = duration;
+    stim.tObsBegin = tObsBegin;
+    stim.tObsEnd = tObsEnd;
+    is >> steps;
+    stim.clear();
+    for ( quint32 i = 0; i < steps; i++ ) {
+        Stimulation::Step s;
+        is >> s.ramp >> t >> V;
+        s.t = t;
+        s.V = V;
+        stim.insert(stim.end(), std::move(s));
+    }
+    return is;
+}
+
+QDataStream &operator<<(QDataStream &os, const WaveStats &stats)
+{
+    os << qint32(stats.bubbles) << double(stats.fitness) << qint32(stats.best.cycles) << double(stats.best.tEnd);
+    return os;
+}
+
+QDataStream &operator>>(QDataStream &is, WaveStats &stats)
+{
+    double tEnd, fitness;
+    qint32 cycles, bubbles;
+    is >> bubbles >> fitness >> cycles >> tEnd;
+    stats.bubbles = bubbles;
+    stats.fitness = fitness;
+    stats.best.cycles = cycles;
+    stats.best.tEnd = tEnd;
+    return is;
+}
--- src/include/streamops.h
+++ src/include/streamops.h
@@ -2,10 +2,21 @@
 #define STREAMOPS_H
 
 #include <QString>
+#include <QDataStream>
 #include <iostream>
+#include "types.h"
 
 std::istream &operator>>(std::istream &is, QString &str);
 std::ostream &operator<<(std::ostream &os, const QString &str);
+
+QDataStream &operator<<(QDataStream &, const MAPElite &);
+QDataStream &operator>>(QDataStream &, MAPElite &);
+
+QDataStream &operator<<(QDataStream &, const Stimulation &);
+QDataStream &operator>>(QDataStream &, Stimulation &);
+
+QDataStream &operator<<(QDataStream &, const WaveStats &);
+QDataStream &operator>>(QDataStream &, WaveStats &);
 
 
 #endif // STREAMOPS_H
