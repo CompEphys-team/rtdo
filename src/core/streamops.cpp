072a148142b30835049991158b2b435fbec00bf2
Felix Kern <kernfel+github@gmail.com>
2017-03-21 15:02:07 +0000

    Simplify QDataStream reading to scalar

--- src/core/streamops.cpp
+++ src/core/streamops.cpp
@@ -38,29 +38,23 @@ QDataStream &operator>>(QDataStream &is, MAPElite &e)
 
 QDataStream &operator<<(QDataStream &os, const Stimulation &stim)
 {
-    os << double(stim.baseV) << double(stim.duration) << double(stim.tObsBegin) << double(stim.tObsEnd);
+    os << stim.baseV << stim.duration << stim.tObsBegin << stim.tObsEnd;
     os << quint32(stim.size());
     for ( const Stimulation::Step &s : stim )
-        os << s.ramp << double(s.t) << double(s.V);
+        os << s.ramp << s.t << s.V;
     return os;
 }
 
 QDataStream &operator>>(QDataStream &is, Stimulation &stim)
 {
-    double baseV, duration, tObsBegin, tObsEnd, t, V;
+    // Note: this is floating-point safe under the assumption that is.floatingPointPrecision matches the writer's setting.
     quint32 steps;
-    is >> baseV >> duration >> tObsBegin >> tObsEnd;
-    stim.baseV = baseV;
-    stim.duration = duration;
-    stim.tObsBegin = tObsBegin;
-    stim.tObsEnd = tObsEnd;
+    is >> stim.baseV >> stim.duration >> stim.tObsBegin >> stim.tObsEnd;
     is >> steps;
     stim.clear();
     for ( quint32 i = 0; i < steps; i++ ) {
         Stimulation::Step s;
-        is >> s.ramp >> t >> V;
-        s.t = t;
-        s.V = V;
+        is >> s.ramp >> s.t >> s.V;
         stim.insert(stim.end(), std::move(s));
     }
     return is;
@@ -68,18 +62,15 @@ QDataStream &operator>>(QDataStream &is, Stimulation &stim)
 
 QDataStream &operator<<(QDataStream &os, const WaveStats &stats)
 {
-    os << qint32(stats.bubbles) << double(stats.fitness) << qint32(stats.best.cycles) << double(stats.best.tEnd);
+    os << qint32(stats.bubbles) << stats.fitness << qint32(stats.best.cycles) << stats.best.tEnd;
     return os;
 }
 
 QDataStream &operator>>(QDataStream &is, WaveStats &stats)
 {
-    double tEnd, fitness;
     qint32 cycles, bubbles;
-    is >> bubbles >> fitness >> cycles >> tEnd;
+    is >> bubbles >> stats.fitness >> cycles >> stats.best.tEnd;
     stats.bubbles = bubbles;
-    stats.fitness = fitness;
     stats.best.cycles = cycles;
-    stats.best.tEnd = tEnd;
     return is;
 }
