56b13064576bad43ca4e38b92b27b952e5a783bf
Felix Kern <kernfel+github@gmail.com>
2018-11-01 12:32:44 +0000

    Moved Quantile fn into util.h

--- src/include/util.h
+++ src/include/util.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 using std::size_t;
 
@@ -47,5 +48,48 @@ public:
 };
 std::istream &operator>>(std::istream &is, QuotedString &str);
 std::ostream &operator<<(std::ostream &os, const QuotedString &str);
+
+
+// Interpolated quartiles -- https://stackoverflow.com/a/37708864
+template<typename T>
+inline double Lerp(T v0, T v1, T t)
+{
+    return (1 - t)*v0 + t*v1;
+}
+
+template<typename T>
+inline std::vector<T> Quantile(const std::vector<T>& inData, const std::vector<T>& probs)
+{
+    if (inData.empty())
+    {
+        return std::vector<T>();
+    }
+
+    if (1 == inData.size())
+    {
+        return std::vector<T>(1, inData[0]);
+    }
+
+    std::vector<T> data = inData;
+    std::sort(data.begin(), data.end());
+    std::vector<T> quantiles;
+
+    for (size_t i = 0; i < probs.size(); ++i)
+    {
+        T poi = Lerp<T>(-0.5, data.size() - 0.5, probs[i]);
+
+        size_t left = std::max(int64_t(std::floor(poi)), int64_t(0));
+        size_t right = std::min(int64_t(std::ceil(poi)), int64_t(data.size() - 1));
+
+        T datLeft = data.at(left);
+        T datRight = data.at(right);
+
+        T quantile = Lerp<T>(datLeft, datRight, poi - left);
+
+        quantiles.push_back(quantile);
+    }
+
+    return quantiles;
+}
 
 #endif // UTIL_H
