696cc9b884eb6ca56c434644fde617eed81c8cd7
Felix Kern <kernfel+github@gmail.com>
2018-11-06 12:04:08 +0000

    Propagate Wavegen sigma adjust to UniversalLibrary, too

--- src/core/wavegen.cpp
+++ src/core/wavegen.cpp
@@ -275,9 +275,11 @@ void Wavegen::propagateAdjustedSigma()
 {
     ExperimentLibrary &exp(session.project.experiment());
     ProfilerLibrary &prof(session.project.profiler());
+    UniversalLibrary &uni(session.project.universal());
     for ( size_t i = 0; i < lib.adjustableParams.size(); i++ ) {
         exp.adjustableParams[i].adjustedSigma = lib.adjustableParams[i].adjustedSigma;
         prof.adjustableParams[i].adjustedSigma = lib.adjustableParams[i].adjustedSigma;
+        uni.adjustableParams[i].adjustedSigma = lib.adjustableParams[i].adjustedSigma;
     }
 }
 
