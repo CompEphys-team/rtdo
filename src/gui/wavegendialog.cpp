0084b5186f372c1a6efb7f44b209134bb345e7a2
Felix Kern <kernfel+github@gmail.com>
2019-01-02 10:27:25 +0000

    Migrated WavegenDialog to Wavegen::bubbleSearch

--- src/gui/wavegendialog.cpp
+++ src/gui/wavegendialog.cpp
@@ -10,21 +10,8 @@ WavegenDialog::WavegenDialog(Session &s, QWidget *parent) :
     ui->setupUi(this);
     this->setWindowFlags(Qt::Window);
 
-    for ( const AdjustableParam &p : session.wavegen().lib.model.adjustableParams )
-        ui->cbSearch->addItem(QString::fromStdString(p.name));
-
-    connect(ui->btnSigadjust, &QPushButton::clicked, &session.wavegen(), &Wavegen::adjustSigmas);
-    connect(ui->btnSearchAll, &QPushButton::clicked, [&](bool){
-        for ( size_t i = 0; i < session.wavegen().lib.model.adjustableParams.size(); i++ ) {
-            session.wavegen().search(i);
-        }
-    });
-    connect(ui->btnSearchOne, &QPushButton::clicked, [&](bool){
-        session.wavegen().search(ui->cbSearch->currentIndex());
-    });
-
-    connect(ui->btnEE, &QPushButton::clicked, &session.wavegen(), &Wavegen::clusterSearch);
-
+    connect(ui->btnBubble, &QPushButton::clicked, &session.wavegen(), &Wavegen::bubbleSearch);
+    connect(ui->btnCluster, &QPushButton::clicked, &session.wavegen(), &Wavegen::clusterSearch);
     connect(ui->btnAbort, &QPushButton::clicked, &session, &Session::abort);
 
     ui->progressPlotter->init(session);
