e529cf3533f8831283f814a7cd4998487473b62a
Felix Kern <kernfel+github@gmail.com>
2019-01-06 17:10:31 +0000

    Added iObservations::duration() shortcut

--- src/include/types.h
+++ src/include/types.h
@@ -121,6 +121,7 @@ struct iObservations
     int stop[maxObs];
 
     inline void operator=(double) {}
+    inline int duration() const { int r=0; for (size_t i=0; i<maxObs; i++) r += stop[i]-start[i]; return r; }
 };
 std::ostream &operator<<(std::ostream&, const iObservations&);
 
