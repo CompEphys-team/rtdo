5bb4dc3e65fe39d6cb7a0ea58bd9f675771439c4
Felix Kern <kernfel+github@gmail.com>
2019-10-11 16:00:44 +0100

    Added GPLv2 license

--- src/core/generateAllNoMain.cpp
+++ src/core/generateAllNoMain.cpp
@@ -1,3 +1,22 @@
+/** RTDO: Closed loop neuron model fitting
+ *  Copyright (C) 2019 Felix Kern <kernfel+github@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**/
+
+
 // GeNN's generateALL.cc has an int main(int, char*[]), because it's meant to be compiled and run separately.
 // This just allows us to compile it into the main project without conflict.
 #define main generateAll
